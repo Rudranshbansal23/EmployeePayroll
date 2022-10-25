a341a0e3d22943e28bc73ce5ca5a2381adf798a2
Rudranshbansal23 <bansalrudransh2@gmail.com>
2022-10-25 16:34:24 +0530

    Added: Program to write file

--- src/main/java/com/blz/EmployeePayroll/Day_27_EmployeePayroll/EmployeePayRollMain.java
+++ src/main/java/com/blz/EmployeePayroll/Day_27_EmployeePayroll/EmployeePayRollMain.java
@@ -1,11 +1,10 @@
 package com.blz.EmployeePayroll.Day_27_EmployeePayroll;
 
-
 public class EmployeePayrollMain {
-    public static void main(String[] args) {
-        EmployeePayRollImpl employeePayrollService = new EmployeePayRollImpl();
-        System.out.println("Welcolme to employee payRoll");
-        employeePayrollService.readEmployeeDataFromConsole();
-        employeePayrollService.writeEmployeeDataInConsole();
-    }
+	public static void main(String[] args) {
+		EmployeePayRollImpl employeePayrollService = new EmployeePayRollImpl();
+		System.out.println("Welcolme to employee payRoll");
+		employeePayrollService.readEmployeeDataFromConsole();
+		employeePayrollService.writeEmployeeDataInConsole();
+	}
 }
--- src/main/java/com/blz/EmployeePayroll/Day_27_EmployeePayroll/Java8WatchServiceExample.java
+++ src/main/java/com/blz/EmployeePayroll/Day_27_EmployeePayroll/Java8WatchServiceExample.java
@@ -80,5 +80,4 @@ public class Java8WatchServiceExample {
 			}
 		}
 	}
-
 }
