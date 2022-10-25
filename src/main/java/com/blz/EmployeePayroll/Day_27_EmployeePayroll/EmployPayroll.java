b591926b1aa858c88cdb2fae348b28ad38f8b5f2
Rudranshbansal23 <bansalrudransh2@gmail.com>
2022-10-25 15:03:56 +0530

    Added:Welcome Message

---
+++ src/main/java/com/blz/EmployeePayroll/Day_27_EmployeePayroll/EmployPayroll.java
@@ -0,0 +1,9 @@
+package com.blz.EmployeePayroll.Day_27_EmployeePayroll;
+
+public class EmployPayroll {
+	public static void main(String[] args) {
+		System.out.println("Welome to Employee Payroll Service");
+
+	}
+
+}
---
+++ src/test/java/com/blz/EmployeePayroll/Day_27_EmployeePayroll/AppTest.java
@@ -0,0 +1,20 @@
+package com.blz.EmployeePayroll.Day_27_EmployeePayroll;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
