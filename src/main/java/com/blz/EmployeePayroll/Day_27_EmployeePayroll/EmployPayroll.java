393b00f3b3eb4de35267662fa39a2fced5ce6f16
Rudranshbansal23 <bansalrudransh2@gmail.com>
2022-10-25 15:11:10 +0530

    Added: Program to Read the data from the Employee Payroll

--- src/main/java/com/blz/EmployeePayroll/Day_27_EmployeePayroll/EmployPayroll.java
+++
@@ -1,9 +0,0 @@
-package com.blz.EmployeePayroll.Day_27_EmployeePayroll;
-
-public class EmployPayroll {
-	public static void main(String[] args) {
-		System.out.println("Welome to Employee Payroll Service");
-
-	}
-
-}
